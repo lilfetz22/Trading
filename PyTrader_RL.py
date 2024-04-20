76f4a3e6f27f089c82dbc2820115599aa7633a23
lilfetz22 <billfetzner@yahoo.com>
2024-04-20 16:30:45 -0400

    added account protection conditionals

--- PyTrader_RL.py
+++ PyTrader_RL.py
@@ -42,7 +42,7 @@ End_Hour = 22
 magicnumber = 2772
 SL_in_pips = 20
 TP_in_pips = 10
-multiplier = 10000  # multiplier for calculating SL and TP, for JPY pairs should have the value of 100
+multiplier = 10_000  # multiplier for calculating SL and TP, for JPY pairs should have the value of 100
 if instrument.find('JPY') >= 0:
     multiplier = 100.0  
 
@@ -53,6 +53,7 @@ Max_Payout_Bool = False
 Max_Payout_Amt = 12_000
 Initial_Acct_Size = 200_000
 News_Trading_Allowed = True
+acct_protection, swap_protection_long, swap_protection_short = False, False, False
 
 ## Other Parameters ##
 date_value_last_bar = 0 
@@ -87,49 +88,17 @@ config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
 brokerInstrumentsLookup = config_instruments(config, "ICMarkets")
+
+
 def get_current_equity_balance():
     # Get current equity and balance
     DynamicInfo = MT.Get_dynamic_account_info()
     for prop in DynamicInfo:
-        if prop == 'equity': # need to check this - I have no idea what this will show up as
+        if prop == 'Account equity': 
             current_acct_equity = DynamicInfo[prop]
-        if prop == 'balance':
+        if prop == 'Account balance':
             current_acct_balance = DynamicInfo[prop]
     return current_acct_equity, current_acct_balance
-# for not using talib
-def calculate_simple_moving_average(series: pd.Series, n: int=20) -> pd.Series:
-    """Calculates the simple moving average"""
-    return series.rolling(n).mean()
-
-log = Logger()
-log.configure()
-
-# settings
-timeframe = 'M5'
-instrument = 'EURUSD'
-server_IP = '127.0.0.1'
-server_port = 1122                                              # check port number
-SL_in_pips = 20
-TP_in_pips = 10
-
-volume = 0.01
-slippage = 5
-magicnumber = 2772
-multiplier = 10000                                              # multiplier for calculating SL and TP, for JPY pairs should have the value of 100
-if instrument.find('JPY') >= 0:
-    multiplier = 100.0  
-sma_period_1 = 9
-sma_period_2 = 16
-date_value_last_bar = 0 
-number_of_bars = 100                 
-
-#   Create simple lookup table, for the demo api only the following instruments can be traded
-brokerInstrumentsLookup = {
-    'EURUSD': 'EURUSD',
-    'AUDCHF': 'AUDCHF',
-    'NZDCHF': 'NZDCHF',
-    'GBPNZD': 'GBPNZD',
-    'USDCAD': 'USDCAD'}
 
 # Define pytrader API
 MT = Pytrader_API()
@@ -144,8 +113,24 @@ if (connection == True):
         ServerTime = MT.Get_broker_server_time()
         if ServerTime.hour == 0 and ServerTime.minute == 0 and ServerTime.second == 0:
             account_equity_at_last_close, account_balance_at_last_close = get_current_equity_balance()
+            print(f'Account equity at close: {account_equity_at_last_close}, Account balance at close: {account_balance_at_last_close}')
             # reset acct_protection, swap_protection_long, swap_protection_short
+            acct_protection, swap_protection_long, swap_protection_short = False, False, False
+        
+        AcctBalDrawdown = (account_balance_at_last_close - current_account_balance) / account_balance_at_last_close
+        AcctEquityDrawdown = (account_equity_at_last_close - current_account_equity) / account_equity_at_last_close
 
+        InstrumentInfo = MT.Get_instrument_info(instrument=instrument)
+        if InstrumentInfo is not None:
+            for prop in InstrumentInfo:
+                if prop == 'swap_long':
+                    swap_long = InstrumentInfo[prop]
+                    if swap_long < 0:
+                        swap_protection_long = True
+                if prop == 'swap_short':
+                    swap_short = InstrumentInfo[prop]
+                    if swap_short < 0:
+                        swap_protection_short = True
 
 
         # retrieve open positions
@@ -157,12 +142,37 @@ if (connection == True):
         if (len(positions_df) > 0):
             for position in positions_df.itertuples():
 
-                if (position.instrument == instrument and position.position_type == 'buy' and position.magic_number == magicnumber):
-                    # tp = position.open_price + TP_in_pips / multiplier
-                    if (actual_bar_info['close'] > tp):
-                        # close the position
-                        MT.Close_position_by_ticket(ticket=position.ticket)
-                        log.debug('trade with ticket ' + str(position.ticket) + ' closed in profit')
+                # account protection if statements
+                if (position.instrument == instrument and position.magic_number == magicnumber and (AcctBalDrawdown > max_Daily_Drawdown_Perc or AcctEquityDrawdown > max_Daily_Drawdown_Perc)):
+                    acct_protection = True
+                    # close the position
+                    MT.Close_position_by_ticket(ticket=position.ticket)
+                    log.debug('trade with ticket ' + str(position.ticket) + ' closed due to daily drawdown protection')
+
+                if (position.instrument == instrument and position.magic_number == magicnumber and (Max_Payout_Bool == True and (current_account_balance - Initial_Acct_Size) > Max_Payout_Amt)):
+                    acct_protection = True
+                    # close the position
+                    MT.Close_position_by_ticket(ticket=position.ticket)
+                    log.debug('trade with ticket ' + str(position.ticket) + ' closed due to max payout - Request payout now! Yay!')
+                # close positions to not allow weekend holds
+                if (position.instrument == instrument and position.magic_number == magicnumber and (ServerTime.hour == 23 & ServerTime.day == 5)):
+                    # close the position
+                    MT.Close_position_by_ticket(ticket=position.ticket)
+                    log.debug('trade with ticket ' + str(position.ticket) + ' closed for no weekend hold')
+
+                # close negative swap positions
+                if (position.instrument == instrument and position.magic_number == magicnumber and ((swap_protection_long == True and position.position_type == 'buy') or
+                                                                                                     (swap_protection_short == True and position.position_type == 'sell'))):
+                    # close the position
+                    MT.Close_position_by_ticket(ticket=position.ticket)
+                    log.debug(f'trade with {position.position_type} ticket {position.ticket} closed due to negative swap protection')
+
+                # if (position.instrument == instrument and position.position_type == 'buy' and position.magic_number == magicnumber):
+                #     # tp = position.open_price + TP_in_pips / multiplier
+                #     if (actual_bar_info['close'] > tp):
+                #         # close the position
+                #         MT.Close_position_by_ticket(ticket=position.ticket)
+                #         log.debug('trade with ticket ' + str(position.ticket) + ' closed in profit')
                         
                 elif (position.instrument == instrument and position.position_type == 'buy' and SL_in_pips > 0.0 and position.magic_number == magicnumber):
                     sl = position.open_price - SL_in_pips / multiplier
@@ -171,12 +181,12 @@ if (connection == True):
                         MT.Close_position_by_ticket(ticket=position.ticket)
                         log.debug('trade with ticket ' + str(position.ticket) + ' closed with stoploss')
                 
-                elif (position.instrument == instrument and position.position_type == 'sell' and position.magic_number == magicnumber):
-                    # tp = position.open_price - TP_in_pips / multiplier
-                    if (actual_bar_info['close'] < tp):
-                        # close the position
-                        MT.Close_position_by_ticket(ticket=position.ticket)
-                        log.debug('trade with ticket ' + str(position.ticket) + ' closed in profit')
+                # elif (position.instrument == instrument and position.position_type == 'sell' and position.magic_number == magicnumber):
+                #     # tp = position.open_price - TP_in_pips / multiplier
+                #     if (actual_bar_info['close'] < tp):
+                #         # close the position
+                #         MT.Close_position_by_ticket(ticket=position.ticket)
+                #         log.debug('trade with ticket ' + str(position.ticket) + ' closed in profit')
                 elif (position.instrument == instrument and position.position_type == 'sell' and SL_in_pips > 0.0 and position.magic_number == magicnumber):
                     sl = position.open_price + SL_in_pips / multiplier
                     if (actual_bar_info['close'] > sl):
@@ -197,14 +207,6 @@ if (connection == True):
             df = pd.DataFrame(bars)
             df.rename(columns = {'tick_volume':'volume'}, inplace = True)
             df['date'] = pd.to_datetime(df['date'], unit='s')
-            # add the 2x sma's to
-            # using talib here
-            # add the 2x sma's to
-            # using talib here or not
-            #df.insert(0, column='sma_1', value=ta.SMA(df['close'], timeperiod=sma_period_1))
-            #df.insert(0, column='sma_2', value=ta.SMA(df['close'], timeperiod=sma_period_2))
-            df.insert(0, column='sma_1', value=calculate_simple_moving_average(df['close'], n = sma_period_1))
-            df.insert(0, column='sma_2', value=calculate_simple_moving_average(df['close'], n = sma_period_2))
 
             index = len(df) - 2
             # conditions will be checked on bar [index] and [index-1]
